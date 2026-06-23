c3c0984c79ecf2868f0d5035c1ee3d652f87c3cd
Sureshkappala <144470888+Sureshkappala@users.noreply.github.com>
2026-06-23 15:10:51 +0530

    Fix mobile menu icon and scroll lock

--- js/main.js
+++ js/main.js
@@ -25,5 +25,8 @@ const menuToggle = document.querySelector(".menu-toggle");
 const navLinks = document.querySelector(".nav-links");
 
 menuToggle.addEventListener("click", () => {
+
     navLinks.classList.toggle("active");
+    document.body.classList.toggle("menu-open");
+
 });
